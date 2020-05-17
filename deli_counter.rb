f83ad38bf1602108e682451f06dd5d09281a530c
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-17 00:56:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,12 @@
 def line(katz_deli)
   if katz_deli.empty? 
     puts "The line is currently empty."
-  elsif 
-    puts "The line is currently: #{katz_deli}. "
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
   
 end
